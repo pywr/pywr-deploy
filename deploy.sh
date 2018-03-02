db6c529e146ff27971a6eb5e811bd0abd82208ac
James Tomlinson <tomo.bbe@gmail.com>
2018-03-02 20:05:06 +0000

    Added deploy.sh script for travis.

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+
+if [ ${TRAVIS_OS_NAME} == "osx" ]; then
+  anaconda -t ${CONDA_UPLOAD_TOKEN} upload -u pywr ${HOME}/miniconda3/conda-bld/osx-64/*.tar.bz2 --force
+else
+  anaconda -t ${CONDA_UPLOAD_TOKEN} upload -u pywr ${HOME}/miniconda3/conda-bld/linux-64/*.tar.bz2 --force
+fi
